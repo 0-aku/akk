e87f751c9e95bf81b75fd733e4fe9556af0d6566
fixik-nolik <nolik@in-wave.com>
2026-01-03 13:56:36 +0300

    change psql user

--- app/config/database.js
+++ app/config/database.js
@@ -1,7 +1,7 @@
 ({
   host: process.env.DB_HOST || '127.0.0.1',
   port: 5432,
-  database: 'app',
-  user: 'marcus',
-  password: 'marcus',
+  database: 'akk',
+  user: 'nolik',
+  password: 'Nslsom!',
 });
--- app/config/scale.js
+++ app/config/scale.js
@@ -1,5 +1,5 @@
 ({
-  cloud: 'PrivateCloud',
+  cloud: 'AkkCloud',
   server: '1',
   instance: 'standalone',
   token: 'ap14-me0b/6jtl9i:8d7$k8901d556*g',
